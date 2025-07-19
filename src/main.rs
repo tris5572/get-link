25e2aeeaedbead23950eb0463581f54a8b7d3198
tris <nexus4fortune@gmail.com>
2025-07-19 10:34:40 +0900

    feat: `clap` でコマンドライン引数を取得する処理を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+/// 指定されたURLからリンクを抽出するCLIツール
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// リンクを抽出する対象のURL
+    #[arg(short, long)]
+    url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("URL: {}", args.url);
 }
